e52812e5c2863c89970e759725be924881230571
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-05 19:25:27 +0530

    edit script

--- venvactivator.bat
+++ venvactivator.bat
@@ -1,2 +1,5 @@
-C:\Users\kishl\Downloads\venv\venvtest1\Scripts\activate
+cd C:\Users\kishl\Downloads\venv\venvtest1\Scripts\activate
 cd C:\Users\kishl\Downloads\venv\venvtest1
+
+
+@REM @dhruv make one branch for yourself
