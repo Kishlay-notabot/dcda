b29be560537300a75d98b2294b7890178a6d379e
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2023-12-09 18:21:50 +0530

    install tesseract and switch to linux for path management

---
+++ tesseractr/main.py
@@ -0,0 +1,27 @@
+from PIL import Image
+import pytesseract
+pytesseract.pytesseract.tesseract_cmd = r''
+
+
+# Read the image
+image = Image.open("image.png")
+
+# Set PSM to 6 for line segmentation
+pytesseract.config["psm"] = 6
+
+# Extract the lines
+boxes = pytesseract.image_to_boxes(image)
+lines = []
+for b in boxes.splitlines():
+    # Extract the coordinates of the bounding box
+    x, y, w, h = [int(v) for v in b.split()]
+    # Create a dictionary to store the line information
+    line_info = {"bounding_box": (x, y, w, h)}
+    lines.append(line_info)
+
+# Process the lines further
+# (optional: perform OCR on each line, save line information, etc.)
+
+# Print the extracted lines
+for line in lines:
+    print(f"Line: {line['bounding_box']}")
