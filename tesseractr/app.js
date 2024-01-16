ac1c0f3ee4d9adedc8a1567376ce0835932a91e6
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-16 13:12:10 +0530

    add 1 if we need just one worker

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,3 +1,3 @@
 const Ts =  require("tesseract.js")
-Ts.recognize('./tss.jpg','eng',{logger: e =>console.log(e)})
+Ts.recognize('./tss.jpg','eng',{logger: e =>console.log(e)},1)
     .then(out=> console.log(out))
