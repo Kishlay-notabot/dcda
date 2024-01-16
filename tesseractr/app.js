1edd068b9954244971de1f27ad97c982dd394c1a
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-16 13:25:25 +0530

    text dump

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,3 +1,19 @@
-const Ts =  require("tesseract.js")
-Ts.recognize('./tss.jpg','eng',1,{logger: e =>console.log(e)})
-    .then(out=> console.log(out))
+const Tesseract=require('tesseract.js');
+const fs=require('fs').promises;
+
+async function performOCR() {
+  try {
+    const {data: {text}, confidence} = await Tesseract.recognize('./tss.jpg', 'hin', {logger: info => console.log(info)});
+
+    console.log('Recognized Text:', text);
+    console.log('Confidence Values:', confidence);
+
+    await fs.writeFile('output.txt', text);
+    console.log('Recognized text saved to output.txt');
+
+  } catch (error) {
+    console.error('Error during OCR:', error);
+  }
+}
+
+performOCR();
