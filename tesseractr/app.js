f43a4d99ffe08203fc3d1657c2829bc888501ed2
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-16 13:15:00 +0530

    edit

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,3 +1,3 @@
 const Ts =  require("tesseract.js")
-Ts.recognize('./tss.jpg','eng',{logger: e =>console.log(e)},1)
+Ts.recognize('./tss.jpg','eng',1,{logger: e =>console.log(e)})
     .then(out=> console.log(out))
