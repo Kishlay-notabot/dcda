7f5bcb35b19460420efbb5421f48d332f2308129
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-18 21:19:59 +0530

    jpn loaded

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,6 +1,6 @@
 const Ts = require("tesseract.js");
 
-Ts.recognize('./tss.jpg', 'hin+eng', {
+Ts.recognize('./tss.jpg', 'hin+eng+jpn', {
   logger: e => console.log(e),
   tessedit_pageseg_mode: Ts.PSM.SINGLE_CHAR,
 })
