6ec5312b18d2d481e1d15f4c44f39602e9b4e1c3
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-27 14:29:16 +0530

    try whitelist, not working

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,7 +1,8 @@
 const Ts = require("tesseract.js");
 
-Ts.recognize('./processed_GARHvUFWwAAVF9N_binary.png', 'hin+eng+jpn', {
+Ts.recognize('./processed_GARHvUFWwAAVF9N_binary.png', 'eng', {
   logger: e => console.log(e),
   tessedit_pageseg_mode: Ts.PSM.SINGLE_CHAR,
+  tessedit_char_whitelist: '0123456789',
 })
   .then(out => console.log(out))
