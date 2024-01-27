1dcc7bb6f94f41102aee7393692d5c42fc843955
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-27 17:07:33 +0530

    check package-lock

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,4 +1,22 @@
-const Ts =  require("tesseract.js")
-Ts.recognize('./tss.jpg','eng',{logger: e =>console.log(e),
-  tessedit_char_whitelist: '0123456789'},1)
-    .then(out=> console.log(out))
+const { createWorker } = require('tesseract.js');
+
+(async () => {
+  const worker = createWorker({
+    logger: info => console.log(info),
+  });
+
+  await worker.load();
+  await worker.loadLanguage('eng');
+  await worker.initialize('eng');
+
+  await worker.setParameters({
+    tessedit_char_whitelist: '0123456789',
+  });
+
+  try {
+    const { data: { text } } = await worker.recognize('./tss.jpg');
+    console.log(text);
+  } finally {
+    await worker.terminate();
+  }
+})();
