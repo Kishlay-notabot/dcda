d3f0671f3070e85c675ce0463e7c95a67332af55
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-01-31 13:46:59 +0530

    worker init

--- tesseractr/app.js
+++ tesseractr/app.js
@@ -1,22 +1,16 @@
-const { createWorker } = require('tesseract.js');
+const { createWorker, createScheduler } = require('tesseract.js');
+
+const scheduler = createScheduler();
+const worker1 = await createWorker('eng');
+const worker2 = await createWorker('eng');
 
 (async () => {
-  const worker = createWorker({
-    logger: info => console.log(info),
-  });
-
-  await worker.load();
-  await worker.loadLanguage('eng');
-  await worker.initialize('eng');
-
-  await worker.setParameters({
-    tessedit_char_whitelist: '0123456789',
-  });
-
-  try {
-    const { data: { text } } = await worker.recognize('./tss.jpg');
-    console.log(text);
-  } finally {
-    await worker.terminate();
-  }
+  scheduler.addWorker(worker1);
+  scheduler.addWorker(worker2);
+  /** Add 10 recognition jobs */
+  const results = await Promise.all(Array(10).fill(0).map(() => (
+    scheduler.addJob('recognize', 'https://tesseract.projectnaptha.com/img/eng_bw.png')
+  )))
+  console.log(results);
+  await scheduler.terminate(); // It also terminates all workers.
 })();
