44386c3a2d4eb6055e54844fed1c20e7bb27a0e8
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-02-04 01:50:00 +0530

    add images

--- tesseractr/tm/app.js
+++ tesseractr/tm/app.js
@@ -6,33 +6,41 @@ async function processImages() {
   const folderPath = path.resolve(__dirname, './testing');
 
   try {
+    console.log('Reading files from the specified folder:', folderPath);
     const files = await fs.readdir(folderPath);
 
     if (files.length === 0) {
       throw new Error('No image files found in the specified folder.');
     }
 
+    console.log('Number of image files found:', files.length);
+
     const imageArr = files.map(file => path.join(folderPath, file));
 
     const scheduler = createScheduler();
 
     const workerGen = async () => {
+      console.log('Creating a worker.');
       const worker = await createWorker("hin", 1, { logger: m => { console.log(m) }, cachePath: "." });
       scheduler.addWorker(worker);
     }
 
     const workerN = 2;
 
+    console.log(`Creating ${workerN} workers.`);
     const resArr = Array(workerN);
     for (let i = 0; i < workerN; i++) {
       resArr[i] = workerGen();
     }
     await Promise.all(resArr);
 
+    console.log('Processing images and performing OCR:');
+
     const results = [];
 
     for (let i = 0; i < imageArr.length; i++) {
       const imagePath = imageArr[i];
+      console.log(`Processing image ${i + 1}/${imageArr.length}: ${imagePath}`);
       const out = await scheduler.addJob('recognize', imagePath);
       const result = {
         imageName: path.basename(imagePath),
@@ -47,8 +55,11 @@ async function processImages() {
 
     await scheduler.terminate(); //terminate workers
 
+    console.log('OCR processing completed.');
+
     // Save the results as a JSON file
     const jsonFilePath = path.resolve(__dirname, 'ocr_results.json');
+    console.log('Exporting OCR results to JSON file:', jsonFilePath);
     await fs.writeFile(jsonFilePath, JSON.stringify(results, null, 2));
 
     console.log('OCR results saved to:', jsonFilePath);
