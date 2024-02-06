b8a6a420eb5d6d0a51623c50165fe603d45466c2
Kishlay-notabot <starlordcosmicfantasy@gmail.com>
2024-02-06 20:53:43 +0530

    this drawing code works

--- tesseractr/tm/crp/crop.js
+++ tesseractr/tm/crp/crop.js
@@ -1,29 +1,40 @@
 const { createCanvas, loadImage } = require('canvas');
 const fs = require('fs');
 
-function drawRectangle() {
-    
-    const canvas = createCanvas();
-    const ctx = canvas.getContext('2d');
+function drawRectanglesFromJson(jsonFilePath) {
+    // Read the JSON file
+    const jsonData = JSON.parse(fs.readFileSync(jsonFilePath, 'utf8'));
 
-    loadImage('tss.jpg').then((image) => {
-        
-        canvas.width = image.width;
-        canvas.height = image.height;
+    jsonData.forEach(({ imageName, words }) => {
+        // Create a canvas
+        const canvas = createCanvas();
+        const ctx = canvas.getContext('2d');
 
-        ctx.drawImage(image, 0, 0);
+        // Load the image
+        loadImage(imageName).then((image) => {
+            // Set canvas size to match the image size
+            canvas.width = image.width;
+            canvas.height = image.height;
 
-        const rectWidth = 100;
-        const rectHeight = 100;
+            // Draw the image on the canvas
+            ctx.drawImage(image, 0, 0);
 
-        const rectLeft = (canvas.width - rectWidth) / 2;
-        const rectTop = (canvas.height - rectHeight) / 2;
+            // Draw rectangles based on the coordinates from JSON
+            words.forEach(({ bbox }) => {
+                const { x0, y0, x1, y1 } = bbox;
 
-        ctx.strokeStyle = 'red';
-        ctx.strokeRect(rectLeft, rectTop, rectWidth, rectHeight);
-        const buffer = canvas.toBuffer('image/png');
-        fs.writeFileSync('output.png', buffer);
+                // Draw the rectangle
+                ctx.strokeStyle = 'red';
+                ctx.strokeRect(x0, y0, x1 - x0, y1 - y0);
+            });
+
+            // Save the canvas as an image
+            const outputFilePath = `output_${imageName}`;
+            const buffer = canvas.toBuffer('image/png');
+            fs.writeFileSync(outputFilePath, buffer);
+        });
     });
 }
 
-drawRectangle();
+const jsonFilePath = 'bbox_data.json'; // Replace with your actual JSON file path
+drawRectanglesFromJson(jsonFilePath);
